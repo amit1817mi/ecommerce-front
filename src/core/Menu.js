ad04c70f84968c4878d90f067c3a51f02d661f5b
Amit Choudhary <amitchoudhary1817@gmail.com>
2022-04-20 15:45:01 +0530

    updated user profile

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { signout, isAuthenticated } from "../auth/index";
+import { itemTotal } from "./cartHelpers";
 
 const isActive = (history, path) => {
     // console.log(history);
@@ -22,6 +23,20 @@ const Menu = ({ history }) => (
 
             <li className="nav-item">
                 <Link className="nav-link" style={isActive(history, '/shop')} to="/shop">Shop</Link>
+            </li>
+
+            <li className="nav-item">
+                <Link
+                    className="nav-link"
+                    style={isActive(history, '/cart')}
+                    to="/cart">
+                    Cart{" "}
+                    <sup>
+                        <small className="cart-badge">
+                            {itemTotal()}
+                        </small>
+                    </sup>
+                </Link>
             </li>
 
             {isAuthenticated() && isAuthenticated().user.role === 0 && (
--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -5,20 +5,20 @@ export const getProducts = (sortBy) => {
     return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
         method: "GET"
     })
-    .then(response => {
-        return response.json();
-    })
-    .catch(err => console.log(err));
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
 }
 
 export const getCategories = () => {
     return fetch(`${API}/categories`, {
         method: "GET"
     })
-    .then(response => {
-        return response.json();
-    })
-    .catch(err => console.log(err));
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
 };
 
 export const getFilteredProducts = (skip, limit, filters = {}) => {
@@ -48,8 +48,73 @@ export const list = params => {
     return fetch(`${API}/products/search?${query}`, {
         method: "GET"
     })
-    .then(response => {
-        return response.json();
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+}
+
+export const read = (productId) => {
+    return fetch(`${API}/product/${productId}`, {
+        method: "GET"
     })
-    .catch(err => console.log(err));
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const listRelated = (productId) => {
+    return fetch(`${API}/products/related/${productId}`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+}
+
+export const getBraintreeClientToken = (userId, token) => {
+    return fetch(`${API}/braintree/getToken/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: 'application/json',
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+}
+
+export const processPayment = (userId, token, paymentData) => {
+    return fetch(`${API}/braintree/payment/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: 'application/json',
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(paymentData)
+    })
+        .then(response => {
+            return response.json();
+        }).catch(err => console.log(err));
+}
+
+export const createOrder = (userId, token, createOrderData) => {
+    return fetch(`${API}/order/create/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: 'application/json',
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({order: createOrderData})
+    })
+        .then(response => {
+            return response.json();
+        }).catch(err => console.log(err));
 }
